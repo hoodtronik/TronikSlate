5c19c2622e50677230d9c81f2f9233dd3b306b15
hoodTRONIK <76516088+hoodtronik@users.noreply.github.com>
2026-03-03 19:48:04 -0500

    Harden install/update scripts and make reset truly clean

--- update.js
+++ update.js
@@ -1,14 +1,21 @@
 module.exports = {
-  run: [{
-    method: "shell.run",
-    params: {
-      message: "git pull"
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        message: "git pull --ff-only"
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        path: "app",
+        message: [
+          "git fetch --all --prune",
+          "git pull --ff-only",
+          "npm ci"
+        ]
+      }
     }
-  }, {
-    method: "shell.run",
-    params: {
-      path: "app",
-      message: "git pull"
-    }
-  }]
+  ]
 }
